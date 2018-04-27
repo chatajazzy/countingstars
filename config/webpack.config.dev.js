cf7f5c7c16acde40a64fa23ba40c1e593e40c8b9
Unknown <chatajazzy@interia.pl>
2018-04-27 22:14:26 +0200

    Add sass config to webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -154,36 +154,8 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.(s)css$/,
-            use: [
-              require.resolve('style-loader'),
-              {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1
-                }
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9' // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009'
-                    })
-                  ]
-                }
-              }
-            ]
+            test: /.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
