76dd53e0bc8bb101f08a27bc58e865c01d3fa3f4
Unknown <chatajazzy@interia.pl>
2018-04-28 16:59:38 +0200

    Add BEM to app component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Game from './components/Game';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <div id="stars" />
         <div id="stars2" />
         <div id="stars3" />
 
-        <header className="App-header">
-          <h1 className="App-title">Counting Stars Temporary Title</h1>
+        <header className="app__header">
+          <h1 className="app__title">Counting Stars</h1>
         </header>
         <Game />
       </div>
